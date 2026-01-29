e43e011461c80728bf41be1eae5938abb702919f
Dominique-Shanti Bouchez <shanti-pub.git@mildred.fr>
2026-01-29 23:53:27 +0100

    Flag

--- cmd/conductor/service.go
+++ cmd/conductor/service.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -171,12 +170,14 @@ func cmd_service_reload() *flaggy.Subcommand {
 type NegativeBoolFlag struct{ Bool *bool }
 
 func (b *NegativeBoolFlag) Set(s string) error {
-	v, err := strconv.ParseBool(s)
-	if err != nil {
-		err = errors.New("parse error")
+	if s == "" {
+		*b.Bool = false
+		return nil
+	} else {
+		v, err := strconv.ParseBool(s)
+		*b.Bool = !v
+		return err
 	}
-	*b.Bool = !v
-	return err
 }
 
 func (b *NegativeBoolFlag) Get() any { return !bool(*b.Bool) }
